9482b5c2e77150319a16022b0d063cedad0c6544
k1borg32 <253847@edu.p.lodz.pl>
2026-01-18 15:29:28 +0100

    Clean up task4 branch: Remove Task 3 and other unrelated files
    Removed:
    - Makefile.task3
    - All Task 3 source files (Morphology, Segmentation, etc.)
    - Histogram, ImageProcessor, LinearFilters, NonLinearFilters classes
    
    Keeping only Task 4 files:
    - FFT.h, FFT.cpp
    - FrequencyFilters.h, FrequencyFilters.cpp
    - main_task4.cpp
    - Makefile.task4
    - Output images and test images

--- src/NonLinearFilters.h
+++
@@ -1,8 +0,0 @@
-#ifndef NONLINEAR_FILTERS_H
-#define NONLINEAR_FILTERS_H
-
-#include "Utils.h"
-
-CImg<unsigned char> rosenfeld_operator  (const CImg<unsigned char>& src, int P = 1);
-
-#endif
--- src/Segmentation.h
+++
@@ -1,8 +0,0 @@
-﻿#ifndef SEGMENTATION_H
-#define SEGMENTATION_H
-
-#include "Utils.h"
-
-CImg<unsigned char> region_growing(const CImg<unsigned char>& src, int seedX, int seedY, int threshold);
-
-#endif
